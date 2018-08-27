d7061699f9770b5d9e9e7d5050e6d886069d6a8f
LGC <42516079+LGCry@users.noreply.github.com>
2018-08-28 00:20:27 +0900

    0828

---
+++ src/zedwallet/ZedWallet.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2018, The LGCoin Developers
+// Please see the included LICENSE file for more information.
+
+#pragma once
+
+#include <zedwallet/ColouredMsg.h>
+#include <zedwallet/Types.h>
+
+enum Action {Open, Generate, Import, SeedImport, ViewWallet};
+
+Action getAction(Config &config);
+
+void welcomeMsg();
+
+void run(CryptoNote::WalletGreen &wallet, CryptoNote::INode &node,
+         Config &config);
+
+void inputLoop(std::shared_ptr<WalletInfo> &walletInfo,
+               CryptoNote::INode &node);
+
+bool shutdown(CryptoNote::WalletGreen &wallet, CryptoNote::INode &node,
+              bool &alreadyShuttingDown);
+
+std::string getInputAndDoWorkWhileIdle(std::shared_ptr<WalletInfo> &walletInfo);
+
+std::string getCommand(std::shared_ptr<WalletInfo> &walletInfo);
+
+Maybe<std::shared_ptr<WalletInfo>> handleAction(CryptoNote::WalletGreen &wallet,
+                                                Action action, Config &config);
+
